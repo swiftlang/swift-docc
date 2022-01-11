bdb481dc3f48d293d76195ea77d41fcaab374095
Ethan Kusters <ekusters@apple.com>
2022-01-10 20:48:23 -0800

    [5.6] Add Static Hosting Support (#56)
    * Add Static Hosting Support (#44)
    
    Relevant forums discussion: https://forums.swift.org/t/support-hosting-docc-archives-in-static-hosting-environments/53572.
    
    Resolves rdar://70800606.
    
    Co-authored-by: Ethan Kusters <ekusters@apple.com>
    
    * Sort output to avoid relying on the undefined order of directory content (#57)
    
    Co-authored-by: Steve Scott (Scotty) <macdevnet@users.noreply.github.com>
    Co-authored-by: David Rönnqvist <ronnqvist@apple.com>

--- Tests/SwiftDocCUtilitiesTests/ConvertActionTests.swift
+++ Tests/SwiftDocCUtilitiesTests/ConvertActionTests.swift
@@ -2240,14 +2240,14 @@ class ConvertActionTests: XCTestCase {
         
         try action.performAndHandleResult()
         XCTAssertFalse(
-            testDataProvider.fileExists(atPath: targetDirectory.appendingPathComponent("data").path)
+            testDataProvider.fileExists(atPath: targetDirectory.appendingPathComponent(NodeURLGenerator.Path.dataFolderName).path)
         )
         
         enableFeatureFlag(\.isExperimentalObjectiveCSupportEnabled)
         
         try action.performAndHandleResult()
         XCTAssertTrue(
-            testDataProvider.fileExists(atPath: targetDirectory.appendingPathComponent("data").path)
+            testDataProvider.fileExists(atPath: targetDirectory.appendingPathComponent(NodeURLGenerator.Path.dataFolderName).path)
         )
     }
     
