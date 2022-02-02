20c289481ff67a2f32efd6999abb6c5ea704de17
Bryan Ricker <978899+bricker@users.noreply.github.com>
2022-02-02 13:32:55 -0800

    feat: add all supported platforms for sortedKeys availability check
    Sort JSON output for any platform that supports it
    
    This change outputs deterministic JSON files into the doccarchive,
    so documentation archives that are committed to source control
    only change when the APIs have changed.
    
    code review; typo fix
    
    tests: skip sorted keys test on unsupported platforms
    
    fix: move json key sorting behind prettyPrint flag
    
    revert: outputFormatting setter, spacing
    
    chore: fix unnecessary outputFormatting line, spacing
    
    docs: update DOCC_JSON_PRETTYPRINT doc

--- Tests/SwiftDocCTests/Converter/RenderNodeCodableTests.swift
+++ Tests/SwiftDocCTests/Converter/RenderNodeCodableTests.swift
@@ -95,6 +95,20 @@ class RenderNodeCodableTests: XCTestCase {
         }
     }
     
+    func testSortedKeys() throws {
+        guard #available(macOS 10.13, iOS 11.0, watchOS 4.0, tvOS 11.0, *) else {
+            throw XCTSkip("Skipped on platforms that don't support JSONEncoder.OutputFormatting.sortedKeys")
+        }
+
+        // When prettyPrint is enabled, keys are sorted
+        let encoderPretty = RenderJSONEncoder.makeEncoder(prettyPrint: true)
+        XCTAssertTrue(encoderPretty.outputFormatting.contains(.sortedKeys))
+
+        // When prettyPrint is disabled, keys are not sorted
+        let encoderNotPretty = RenderJSONEncoder.makeEncoder(prettyPrint: false)
+        XCTAssertFalse(encoderNotPretty.outputFormatting.contains(.sortedKeys))
+    }
+
     func testEncodesVariantOverridesSetAsProperty() throws {
         var renderNode = bareRenderNode
         renderNode.variantOverrides = VariantOverrides(values: [testVariantOverride])
