8eeaf0b5046fb727e65190fc1c6f4107f2e8840a
Ethan Kusters <ekusters@apple.com>
2022-06-01 12:23:24 -0700

    Use `python3` in build-script-helper
    Python2 is no longer supported by the Swift project.

--- build-script-helper.py
+++ build-script-helper.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
