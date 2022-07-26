4682dd10b3dc93091730ab47fab7c9fbd2525fd0
Kyle <kyle201817146@gmail.com>
2022-07-26 17:16:25 +0800

    Fix article abstract section issue

--- Tests/SwiftDocCTests/Model/DocumentationMarkupTests.swift
+++ Tests/SwiftDocCTests/Model/DocumentationMarkupTests.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -158,6 +158,21 @@ class DocumentationMarkupTests: XCTestCase {
             XCTAssertEqual(expected, model.abstractSection?.content.map({ $0.detachedFromParent.debugDescription() }).joined(separator: "\n"))
         }
 
+        // Contains a HTMLBlock before the abstract
+        do {
+            let source = """
+            # Title
+            <!--Line a-->
+            Line b
+            ## Hello, world!
+            Discussion content.
+            """
+            let expected = """
+            Text \"Line b\"
+            """
+            let model = DocumentationMarkup(markup: Document(parsing: source, options: .parseBlockDirectives))
+            XCTAssertEqual(expected, model.abstractSection?.content.map{ $0.detachedFromParent.debugDescription() }.joined(separator: "\n"))
+        }
     }
     
     func testDeprecation() throws {
