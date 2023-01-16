aacea9ea54eb300e744e67a635d623f1141fcf08
Franklin Schrans <fschrans@apple.com>
2023-01-16 17:16:58 +0000

    Add @SupportedLanguage directive for articles
    Adds support for the `@SupportedLanguage` directive, which can be used
    in article files to indicate what languages an article is available in,
    rather than using the languages that the module being documented is
    available in.
    
    rdar://102091191

--- Sources/SwiftDocC/Model/SourceLanguage.swift
+++ Sources/SwiftDocC/Model/SourceLanguage.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2023 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -133,6 +133,7 @@ public struct SourceLanguage: Hashable, Codable {
         id: "occ",
         idAliases: [
             "objective-c",
+            "objc",
             "c", // FIXME: DocC should display C as its own language (github.com/apple/swift-docc/issues/169).
         ],
         linkDisambiguationID: "c"
---
+++ Tests/SwiftDocCTests/Test Bundles/MixedLanguageFrameworkWithArticlesUsingSupportedLanguages.docc/ArticleWithoutSupportedLanguages.md
@@ -0,0 +1,5 @@
+# ArticleWithoutSupportedLanguages
+
+This article doesn't specify a `@SupportedLanguages` directive.
+
+<!-- Copyright (c) 2023 Apple Inc and the Swift Project authors. All Rights Reserved. -->
