fb643e33a76819e345370da6c7df1612326ef9c5
QuietMisdreavus <QuietMisdreavus@users.noreply.github.com>
2023-04-05 13:18:59 -0600

    update branch dependencies to release/5.9 (#533)

--- Package.swift
+++ Package.swift
@@ -115,8 +115,8 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     // Building standalone, so fetch all dependencies remotely.
     package.dependencies += [
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMinor(from: "2.31.2")),
-        .package(name: "swift-markdown", url: "https://github.com/apple/swift-markdown.git", .branch("main")),
-        .package(name: "CLMDB", url: "https://github.com/apple/swift-lmdb.git", .branch("main")),
+        .package(name: "swift-markdown", url: "https://github.com/apple/swift-markdown.git", .branch("release/5.9")),
+        .package(name: "CLMDB", url: "https://github.com/apple/swift-lmdb.git", .branch("release/5.9")),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.1")),
         .package(name: "SymbolKit", url: "https://github.com/apple/swift-docc-symbolkit", .branch("release/5.9")),
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMinor(from: "1.1.2")),
