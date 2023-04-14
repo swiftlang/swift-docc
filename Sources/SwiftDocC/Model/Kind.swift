d966b9ab3f1681adbedda7d1d2c75acb960faa83
QuietMisdreavus <QuietMisdreavus@users.noreply.github.com>
2023-04-14 09:48:37 -0600

    drop an "Extended Symbol" page when its children are curated elsewhere (#541)
    rdar://107729630
    
    Bundles with extension block symbols generate placeholder "Extended Symbol"
    pages to automatically curate extension symbols. However, if the symbols are
    then curated elsewhere, the "Extended Symbol" pages are left confusingly
    empty. Drop these pages if they have no child pages remaining.
    
    An "Extended Symbol" page will remain if an extension markdown file is
    provided for it, even if its children are curated elsewhere.
    
    Links to these dropped pages will generate a warning and be converted to code
    voice in the final rendering. References in automatically generated
    symbol-relationship page sections will not generate a warning, but they will
    be converted to code voice as well.

--- Sources/SwiftDocC/Model/Kind.swift
+++ Sources/SwiftDocC/Model/Kind.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2023 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -204,4 +204,14 @@ extension DocumentationNode.Kind {
         // Other
         .keyword, .restAPI, .tag, .propertyList, .object
     ]
+
+    /// Returns whether this symbol kind is a synthetic "Extended Symbol" symbol kind.
+    public var isExtendedSymbolKind: Bool {
+        switch self {
+        case .extendedClass, .extendedModule, .extendedProtocol, .extendedStructure, .extendedEnumeration, .unknownExtendedType:
+            return true
+        default:
+            return false
+        }
+    }
 }
