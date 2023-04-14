8eb29e92b7dcf8b3aec495f909cb5f37689c3ec9
QuietMisdreavus <QuietMisdreavus@users.noreply.github.com>
2023-04-14 14:52:49 -0600

    drop an "Extended Symbol" page when its children are curated elsewhere (#556)
    rdar://107729630
    
    Bundles with extension block symbols generate placeholder "Extended Symbol"
    pages to automatically curate extension symbols. However, if the symbols are
    then curated elsewhere, the "Extended Symbol" pages are left confusingly
    empty. Drop these pages if they have no child pages remaining.
    
    An "Extended Symbol" page will remain if an extension markdown file is
    provided for it, even if its children are curated elsewhere.
    
    Links to these dropped pages will generate a warning and be converted to code
    voice in the final rendering. References in automatically generated
    symbol-relationship page sections will not generate a warning, but they will
    be converted to code voice as well.

--- Sources/SwiftDocC/Semantics/MarkupReferenceResolver.swift
+++ Sources/SwiftDocC/Semantics/MarkupReferenceResolver.swift
@@ -26,6 +26,19 @@ fileprivate func unknownSnippetSliceProblem(snippetPath: String, slice: String, 
     return Problem(diagnostic: diagnostic, possibleSolutions: [])
 }
 
+fileprivate func removedLinkDestinationProblem(reference: ResolvedTopicReference, source: URL?, range: SourceRange?, severity: DiagnosticSeverity) -> Problem {
+    var solutions = [Solution]()
+    if let range = range, reference.pathComponents.count > 3 {
+        // The first three path components are "/", "documentation", and the module name, so drop those
+        let pathRemainder = reference.pathComponents[3...]
+        solutions.append(.init(summary: "Use a plain code span instead of a symbol link", replacements: [
+            .init(range: range, replacement: "`\(pathRemainder.joined(separator: "/"))`")
+        ]))
+    }
+    let diagnostic = Diagnostic(source: source, severity: severity, range: range, identifier: "org.swift.docc.removedExtensionLinkDestination", summary: "The topic \(reference.path.singleQuoted) is an empty extension page and cannot be linked to.", explanation: "This extension symbol has had all its children curated and has been removed.")
+    return Problem(diagnostic: diagnostic, possibleSolutions: solutions)
+}
+
 /**
  Rewrites a ``Markup`` tree to resolve ``UnresolvedTopicReference`s using a ``DocumentationContext``.
  */
@@ -56,7 +69,10 @@ struct MarkupReferenceResolver: MarkupRewriter {
             // If the linked node is part of the topic graph,
             // verify that linking to it is enabled, else return `nil`.
             if let node = context.topicGraph.nodeWithReference(resolved) {
-                guard context.topicGraph.isLinkable(node.reference) else {
+                if node.isEmptyExtension {
+                    problems.append(removedLinkDestinationProblem(reference: resolved, source: source, range: range, severity: severity))
+                    return nil
+                } else if !context.topicGraph.isLinkable(node.reference) {
                     problems.append(disabledLinkDestinationProblem(reference: resolved, source: source, range: range, severity: severity))
                     return nil
                 }
