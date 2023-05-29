6a6db7662ce3d72046c2d0ccdde31245b6a54cbb
Saleem Abdulrasool <compnerd@compnerd.org>
2023-05-29 16:43:31 -0700

    build: fix dependencies
    We need this to use the new initialiser `init(url:exact:)` for updating
    the standalone build to match the CI build.
    
    When building in CI, we would build against swift-crypto 2.5.0 as per
    update-checkout-config.json in apple/swift.  Adjust the standalone
    builds to match.
    
    When building on CI, we would build against swift-argument-parser 1.2.2
    as per update-checkout-config.json in apple/swift.  Make the standalone
    builds match.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 /*
  This source file is part of the Swift.org open source project
 
@@ -133,11 +133,11 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     // Building standalone, so fetch all dependencies remotely.
     package.dependencies += [
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMinor(from: "2.31.2")),
-        .package(name: "swift-markdown", url: "https://github.com/apple/swift-markdown.git", .branch("main")),
-        .package(name: "CLMDB", url: "https://github.com/apple/swift-lmdb.git", .branch("main")),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.1")),
-        .package(name: "SymbolKit", url: "https://github.com/apple/swift-docc-symbolkit", .branch("main")),
-        .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMinor(from: "1.1.2")),
+        .package(name: "swift-markdown", url: "https://github.com/apple/swift-markdown.git", branch: "main"),
+        .package(name: "CLMDB", url: "https://github.com/apple/swift-lmdb.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-argument-parser", exact: Version(1, 2, 2)),
+        .package(name: "SymbolKit", url: "https://github.com/apple/swift-docc-symbolkit", branch: "main"),
+        .package(url: "https://github.com/apple/swift-crypto.git", exact: Version(2, 5, 0)),
     ]
     
     // SwiftPM command plugins are only supported by Swift version 5.6 and later.
