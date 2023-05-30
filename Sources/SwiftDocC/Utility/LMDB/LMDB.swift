b279723892d86c7664daea92de84e0bca65edb39
Saleem Abdulrasool <compnerd@compnerd.org>
2023-05-30 09:03:36 -0700

    build: fix dependencies (#599)
    * build: fix dependencies
    
    We need this to use the new initialiser `init(url:exact:)` for updating
    the standalone build to match the CI build.
    
    When building in CI, we would build against swift-crypto 2.5.0 as per
    update-checkout-config.json in apple/swift.  Adjust the standalone
    builds to match.
    
    When building on CI, we would build against swift-argument-parser 1.2.2
    as per update-checkout-config.json in apple/swift.  Make the standalone
    builds match.
    
    * - Update Package.swift for Swift 5.6 minimum (as already required by NIO).
    - Remove deprecated usages and bump the dependency version requirements for NIO, ArgumentParser, and SwiftCrypto (most especially the latter so that updates in the 2.x series are available).
    - Remove old #if swift conditionals no longer required
    - Package.resolved now uses the version 2 format
    
    * The platform-specific handling for conforming Array to LMDBData is still required.
    
    ---------
    
    Co-authored-by: Gwynne Raskind <gwynne@vapor.codes>

--- Sources/SwiftDocC/Utility/LMDB/LMDB.swift
+++ Sources/SwiftDocC/Utility/LMDB/LMDB.swift
@@ -81,19 +81,11 @@ extension Data: LMDBData {
         self = Data.init(bytes: data.baseAddress!, count: data.count)
     }
 
-#if swift(>=5.0)
     public func read<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R{
         return try self.withUnsafeBytes({ (ptr) -> R in
             return try body(ptr)
         })
     }
-#else
-    public func read<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R{
-        return try self.withUnsafeBytes({ (ptr) -> R in
-            return try body(UnsafeRawBufferPointer(start: ptr, count: self.count))
-        })
-    }
-#endif
 }
 
 extension String: LMDBData {
@@ -106,10 +98,10 @@ extension String: LMDBData {
     }
 }
 
-#if !os(Linux) && !os(Android)
-// This is required for macOS and Swift 4.2, for Linux the default implementation works as expected.
+// This is required for macOS, for Linux the default implementation works as expected.
 extension Array: LMDBData where Element: FixedWidthInteger {
     
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
     public init?(data: UnsafeRawBufferPointer) {
         var array = Array<Element>(repeating: 0, count: data.count / MemoryLayout<Element>.stride)
         _ = array.withUnsafeMutableBytes { data.copyBytes(to: $0) }
@@ -120,11 +112,9 @@ extension Array: LMDBData where Element: FixedWidthInteger {
         let data = self.withUnsafeBufferPointer { Data(buffer: $0) }
         return try data.read(body)
     }
+#endif
 
 }
-#else
-extension Array: LMDBData where Element: FixedWidthInteger {}
-#endif
 
 extension Bool: LMDBData {}
 extension Int: LMDBData {}
