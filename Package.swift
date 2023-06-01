e5894d4af02a0c7a421c8405edc0e31438c560f9
Saleem Abdulrasool <compnerd@compnerd.org>
2023-06-01 07:40:25 -0700

    build: bump swift-docc-plugin to 1.2.0 for Windows
    The Windows support was added in 1.1.0.  Update to the latest release of
    the plugin to support Windows.

--- Package.swift
+++ Package.swift
@@ -163,7 +163,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
         .package(url: "https://github.com/apple/swift-docc-symbolkit", branch: "main"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.2.0"),
     ]
 
 #if !os(Windows)
