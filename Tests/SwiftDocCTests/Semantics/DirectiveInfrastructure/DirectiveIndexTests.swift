f3a1f622b97a444f5c1caa194e093babbac1ba0e
Emily Chen <el_chen@apple.com>
2023-06-12 14:34:15 -0700

    Add authoring support for `@TitleHeading` directive (#611)
    A new `@TitleHeading` metadata directive for customizing the text of a page-title heading (also known as an eyebrow or kick).
    
    `@TitleHeading` accepts an unnamed parameter containing containing the page-title’s heading text.
    
        @Metadata {
          @TitleHeading("Release Notes")
        }
    
    This feature has been pitched on the forums here:
    https://forums.swift.org/t/supporting-more-types-of-documentation-with-swift-docc/59725#titleheading-10
    
    rdar://110662981

--- Tests/SwiftDocCTests/Semantics/DirectiveInfrastructure/DirectiveIndexTests.swift
+++ Tests/SwiftDocCTests/Semantics/DirectiveInfrastructure/DirectiveIndexTests.swift
@@ -48,6 +48,7 @@ class DirectiveIndexTests: XCTestCase {
                 "Tab",
                 "TabNavigator",
                 "TechnologyRoot",
+                "TitleHeading",
                 "TopicsVisualStyle",
                 "Tutorial",
                 "TutorialReference",
