0f3706236f476d3c988918080c746a0ddcca813f
David Rönnqvist <ronnqvist@apple.com>
2023-08-08 13:45:54 -0700

    Remove commented out deprecation

--- Tests/SwiftDocCTests/Infrastructure/DirectiveArgumentWrappedTests.swift
+++ Tests/SwiftDocCTests/Infrastructure/DirectiveArgumentWrappedTests.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2022 Apple Inc. and the Swift project authors
+ Copyright (c) 2022-2023 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -15,7 +15,6 @@ import Markdown
 
 @testable import SwiftDocC
 
-//@available(*, deprecated, message: "This test case is deprecated to silence other deprecation warnings. The tests still run.")
 final class DirectiveArgumentWrappedTests: XCTestCase {
     
     // MARK: - Declarations
