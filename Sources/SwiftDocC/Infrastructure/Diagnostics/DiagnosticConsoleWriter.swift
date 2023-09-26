550d8936f3783cd61547030ec7a177d88202629e
David Rönnqvist <ronnqvist@apple.com>
2023-09-26 10:33:59 -0700

    Add deprecation annotation to deprecated protocol requirement

--- Sources/SwiftDocC/Infrastructure/Diagnostics/DiagnosticConsoleWriter.swift
+++ Sources/SwiftDocC/Infrastructure/Diagnostics/DiagnosticConsoleWriter.swift
@@ -72,6 +72,7 @@ public final class DiagnosticConsoleWriter: DiagnosticFormattingConsumer {
     }
     
     // This is deprecated but still necessary to implement.
+    @available(*, deprecated, renamed: "flush()")
     public func finalize() throws {
         try flush()
     }
