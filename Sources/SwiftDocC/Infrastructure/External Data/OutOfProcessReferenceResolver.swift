3607e00ebc57ad3f6151cc37509de395b8010881
David Rönnqvist <ronnqvist@apple.com>
2024-01-05 10:37:39 +0100

    Fix a crash when configuring a `@DisplayName` for a non-symbol (#783)
    * Preserve all other parsed metadata when article has @DisplayName
    
    rdar://114654594
    
    * Hide metadata initializer that only exist to workaround #468
    
    * Update year in header comments
    
    * Apply suggestions from code review
    
    Co-authored-by: mayaepps <53411851+mayaepps@users.noreply.github.com>
    
    * Clarify test assertion descriptions
    
    ---------
    
    Co-authored-by: mayaepps <53411851+mayaepps@users.noreply.github.com>

--- Sources/SwiftDocC/Infrastructure/External Data/OutOfProcessReferenceResolver.swift
+++ Sources/SwiftDocC/Infrastructure/External Data/OutOfProcessReferenceResolver.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021-2023 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2024 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -276,7 +276,7 @@ public class OutOfProcessReferenceResolver: ExternalReferenceResolver, FallbackR
         // with a render node.
         
         if let topicImages = resolvedInformation.topicImages, !topicImages.isEmpty {
-            let metadata = node.metadata ?? Metadata(originalMarkup: BlockDirective(name: "Metadata", children: []), documentationExtension: nil, technologyRoot: nil, displayName: nil, titleHeading: nil)
+            let metadata = node.metadata ?? Metadata._make(originalMarkup: BlockDirective(name: "Metadata", children: []))
             
             metadata.pageImages = topicImages.map { topicImage in
                 let purpose: PageImage.Purpose
