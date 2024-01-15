ffe5f7564c0c898e65f23228fe0393982647ce09
David Rönnqvist <ronnqvist@apple.com>
2024-01-15 12:35:23 +0100

    Remove memberwise initializers for AutomaticDirectiveConvertible types.
    Types that are automatically convertible aren't intended to be initialized directly.

--- Sources/SwiftDocC/Semantics/Redirect.swift
+++ Sources/SwiftDocC/Semantics/Redirect.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021-2023 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2024 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -32,13 +32,6 @@ public final class Redirect: Semantic, AutomaticDirectiveConvertible {
     ]
     
     static var hiddenFromDocumentation = true
-    
-    init(originalMarkup: BlockDirective, oldPath: URL) {
-        self.originalMarkup = originalMarkup
-        super.init()
-        
-        self.oldPath = oldPath
-    }
     
     @available(*, deprecated, message: "Do not call directly. Required for 'AutomaticDirectiveConvertible'.")
     init(originalMarkup: BlockDirective) {
