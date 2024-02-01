058084ed528f7e7e85fc4042fad4c044910d6e33
Sofia Rodriguez <sofia_rodriguez@apple.com>
2024-02-01 12:44:25 +0100

    Revert "Removed fallback platform constant and custom logic for mac catalyst since it's no longer needed."
    This reverts commit d2613d69755c11912157057d5baa8f62603ec3af.

--- Sources/SwiftDocC/Infrastructure/Symbol Graph/SymbolGraphLoader.swift
+++ Sources/SwiftDocC/Infrastructure/Symbol Graph/SymbolGraphLoader.swift
@@ -199,6 +199,9 @@ struct SymbolGraphLoader {
                         result[defaultAvailability.platformName] = version
                     }
                 })
+            
+            // Defines a fallback for those platforms we don't emit SGFs for.
+            let fallbackPlatforms: [PlatformName:PlatformName] = [:]
 
             // Map all symbols and add default availability for any missing platforms
             let symbolsWithFilledIntroducedVersions = symbolGraph.symbols.mapValues { symbol -> SymbolGraph.Symbol in
@@ -211,7 +214,7 @@ struct SymbolGraphLoader {
                     var newAvailabilityItems = availability.availability.map {
                         $0.fillingMissingIntroducedVersion(
                             from: defaultAvailabilityVersionByPlatform,
-                            fallbackPlatform: nil
+                            fallbackPlatform: fallbackPlatforms[platformName]?.rawValue
                         )
                     }
 
