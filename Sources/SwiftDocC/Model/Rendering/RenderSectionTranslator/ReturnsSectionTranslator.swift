51f2c9dfe34cd49b5a686dbb3ae7e363a7154979
Emily Chen <el_chen@apple.com>
2024-04-15 16:53:41 +0100

    Auto-Capitalize First Word (#880)
    rdar://122167705
    
    When writing a doc comment, it feels intuitive to not capitalise the first word of its description because of the nature of the doc comment.
    
    For example: adding a new parameter would be written as:
    `/// - Parameter testParam: this parameter is just a test parameter to show what this looks like in lowercase.`
    In this example, the sentence in doc comments is natural to write with a lowercase starting word, because the first word (Parameter) is already capitalized. 
    
    This PR auto-capitalizes the first word of a new section or aside. Note that this auto-capitalization only occurs if the first word is all lowercase and contains only characters A-Z, or if the first word contains CharacterSet punctuation characters (e.g. a period, comma, hyphen, semi-colon, colon). This capitalization is also locale specific.
    
    ---------
    
    Co-authored-by: Pat Shaughnessy <pat_shaughnessy@apple.com>

--- Sources/SwiftDocC/Model/Rendering/RenderSectionTranslator/ReturnsSectionTranslator.swift
+++ Sources/SwiftDocC/Model/Rendering/RenderSectionTranslator/ReturnsSectionTranslator.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2024 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -28,7 +28,9 @@ struct ReturnsSectionTranslator: RenderSectionTranslator {
                 return nil
             }
             
-            return ContentRenderSection(kind: .content, content: returnsContent, heading: "Return Value")
+            let capitalizedReturnsContent = [returnsContent[0].withFirstWordCapitalized] + returnsContent[1...]
+            
+            return ContentRenderSection(kind: .content, content: capitalizedReturnsContent, heading: "Return Value")
         }
     }
 }
