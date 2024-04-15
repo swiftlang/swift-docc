c4d3935f80019b748b8b5e01bfe9fde5dfb232f5
Emily Chen <el_chen@apple.com>
2024-04-15 16:54:11 +0100

    Set up diff command line inputs.

--- Sources/SwiftDocCUtilities/ArgumentParsing/Subcommands/ProcessArchive.swift
+++ Sources/SwiftDocCUtilities/ArgumentParsing/Subcommands/ProcessArchive.swift
@@ -18,7 +18,7 @@ extension Docc {
         static var configuration = CommandConfiguration(
             commandName: "process-archive",
             abstract: "Perform operations on documentation archives ('.doccarchive' directories).",
-            subcommands: [TransformForStaticHosting.self, Index.self])
+            subcommands: [TransformForStaticHosting.self, Index.self, Diff.self])
 
     }
 }
