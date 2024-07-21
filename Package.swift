2ff9d919ce2c0e0a61449427eecbb507ab46ed0f
Lam Trinh <>
2024-07-21 15:16:50 +0700

    Update link from https://github.com/apple/swift-lmdb.git to https://github.com/swiftlang/swift-lmdb.git

--- Package.swift
+++ Package.swift
@@ -135,7 +135,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     package.dependencies += [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.53.0"),
         .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
-        .package(url: "https://github.com/apple/swift-lmdb.git", branch: "main"),
+        .package(url: "https://github.com/swiftlang/swift-lmdb.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
         .package(url: "https://github.com/swiftlang/swift-docc-symbolkit", branch: "main"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),
