013ceb4ba67af6e55d9eda64bd444b89ba843e77
Lucca de Mello <luccasukman@gmail.com>
2024-09-19 23:26:36 -0400

    Add support for custom scripts

--- Sources/SwiftDocC/Infrastructure/DocumentationContext.swift
+++ Sources/SwiftDocC/Infrastructure/DocumentationContext.swift
@@ -1784,6 +1784,7 @@ public class DocumentationContext {
     
     private static let supportedImageExtensions: Set<String> = ["png", "jpg", "jpeg", "svg", "gif"]
     private static let supportedVideoExtensions: Set<String> = ["mov", "mp4"]
+    private static let supportedScriptExtensions: Set<String> = ["js"]
 
     // TODO: Move this functionality to ``DocumentationBundleFileTypes`` (rdar://68156425).
     
@@ -1877,6 +1878,14 @@ public class DocumentationContext {
     @available(*, deprecated, renamed: "registeredDownloadsAssets(for:)", message: "registeredDownloadsAssets(for:)' instead. This deprecated API will be removed after 6.2 is released")
     public func registeredDownloadsAssets(forBundleID bundleIdentifier: BundleIdentifier) -> [DataAsset] {
         registeredDownloadsAssets(for: DocumentationBundle.Identifier(rawValue: bundleIdentifier))
+    }
+    
+    /// Returns a list of all the custom scripts that registered for a given `bundleIdentifier`.
+    ///
+    /// - Parameter bundleIdentifier: The identifier of the bundle to return download assets for.
+    /// - Returns: A list of all the custom scripts for the given bundle.
+    public func registeredCustomScripts(forBundleID bundleIdentifier: BundleIdentifier) -> [DataAsset] {
+        return registeredAssets(withExtensions: DocumentationContext.supportedScriptExtensions, forBundleID: bundleIdentifier)
     }
 
     typealias Articles = [DocumentationContext.SemanticResult<Article>]
