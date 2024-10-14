59f3161f8beca2dfec719fe293ddcd9da4ff9ac7
David Rönnqvist <ronnqvist@apple.com>
2024-10-14 17:55:18 +0200

    Update tests that relied on ConvertAction doing validation when run instead of when initialized

--- Tests/SwiftDocCUtilitiesTests/ArgumentParsing/ConvertSubcommandTests.swift
+++ Tests/SwiftDocCUtilitiesTests/ArgumentParsing/ConvertSubcommandTests.swift
@@ -547,21 +547,24 @@ class ConvertSubcommandTests: XCTestCase {
     func testTreatWarningAsError() throws {
         do {
             // Passing no argument should default to the current working directory.
-            let convert = try Docc.Convert.parse([])
+            let convert = try Docc.Convert.parse([
+                testBundleURL.path
+            ])
             let convertAction = try ConvertAction(fromConvertCommand: convert)
             XCTAssertEqual(convertAction.treatWarningsAsErrors, false)
         } catch {
-            XCTFail("Failed to run docc convert without arguments.")
+            XCTFail("Failed to run docc convert with minimal arguments.")
         }
         do {
             // Passing no argument should default to the current working directory.
             let convert = try Docc.Convert.parse([
+                testBundleURL.path,
                 "--warnings-as-errors"
             ])
             let convertAction = try ConvertAction(fromConvertCommand: convert)
             XCTAssertEqual(convertAction.treatWarningsAsErrors, true)
         } catch {
-            XCTFail("Failed to run docc convert without arguments.")
+            XCTFail("Failed to run docc convert with minimal arguments.")
         }
     }
     
