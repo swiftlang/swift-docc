6b4d5a35768b769176143903b07eaf33cd4e3a3f
Sofía Rodríguez <sofia_rodriguez@apple.com>
2024-10-14 18:42:34 +0100

    Add a precondition for declaring a node as beta. (#1055)
    Added a precondition for declaring a node as beta.
    
    For a node to be declared as beta it has to contain at least a single availability item that contains all the already defined characteristics.
    
    This code adds a precondition where at least the node needs to have one availability item to be considered as potentially in beta.
    
    Before this change, if at somewhere in the code, the availability items got removed from the node, the node would be considered as beta.
    
    rdar://137762221
    Co-authored-by: David Rönnqvist <ronnqvist@apple.com>
    
    ---------
    
    Co-authored-by: David Rönnqvist <ronnqvist@apple.com>

--- Sources/SwiftDocC/Model/Rendering/DocumentationContentRenderer.swift
+++ Sources/SwiftDocC/Model/Rendering/DocumentationContentRenderer.swift
@@ -211,11 +211,12 @@ public class DocumentationContentRenderer {
         guard let symbol = node.semantic as? Symbol,
               let currentPlatforms = documentationContext.configuration.externalMetadata.currentPlatforms,
               !currentPlatforms.isEmpty,
-              let symbolAvailability = symbol.availability
+              let symbolAvailability = symbol.availability?.availability,
+              !symbolAvailability.isEmpty // A symbol without availability items can't be in beta.
         else { return false }
 
         // Verify that if current platforms are in beta, they match the introduced version of the symbol
-        for availability in symbolAvailability.availability {
+        for availability in symbolAvailability {
             // If not available on this platform, skip to next platform.
             guard !availability.isUnconditionallyUnavailable, let introduced = availability.introducedVersion else {
                 continue
