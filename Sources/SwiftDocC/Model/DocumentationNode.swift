d3b82835e347d820ff140e3a2f86de245be3b16b
David Rönnqvist <ronnqvist@apple.com>
2026-03-23 16:57:36 +0100

    Deprecate initializer that's only called from tests
    (And update those tests to call other initializers instead)

--- Sources/SwiftDocC/Model/DocumentationNode.swift
+++ Sources/SwiftDocC/Model/DocumentationNode.swift
@@ -742,6 +742,7 @@ public struct DocumentationNode {
     ///   - moduleReference: A reference to the module that the symbol belongs to.
     ///   - article: The documentation extension content for this symbol.
     ///   - engine:The engine that collects any problems encountered during initialization.
+    @available(*, deprecated, message: "This API is only used from tests. It will be removed after 6.5 is released.")
     public init(reference: ResolvedTopicReference, symbol: SymbolGraph.Symbol, platformName: String?, moduleReference: ResolvedTopicReference, article: Article?, engine: DiagnosticEngine) {
         self.reference = reference
         
