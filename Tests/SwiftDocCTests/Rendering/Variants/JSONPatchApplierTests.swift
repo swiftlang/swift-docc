ba06a02ea1e98aa54e941354eb5f7515ab74205d
Franklin Schrans <fschrans@apple.com>
2026-06-12 17:07:30 +0100

    Migrate JSONPatchApplierTests.swift to Swift Testing

--- Tests/SwiftDocCTests/Rendering/Variants/JSONPatchApplierTests.swift
+++ Tests/SwiftDocCTests/Rendering/Variants/JSONPatchApplierTests.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2026 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -9,126 +9,64 @@
 */
 
 import Foundation
-import XCTest
+import Testing
 @testable import SwiftDocC
 
-class JSONPatchApplierTests: XCTestCase {
-    func testReplacesDictionaryValue() throws {
-        XCTAssertEqual(
-            try apply(
-                .replace(pointer: JSONPointer(pathComponents: ["baz"]), encodableValue: "boo"),
-                to: Model(baz: "qux", foo: [])
-            ),
-            Model(
-                baz: "boo",
-                foo: []
-            )
-        )
+struct JSONPatchApplierTests {
+    @Test(arguments: [
+        (
+            input: Model(baz: "qux", foo: []),
+            patch: JSONPatchOperation.replace(pointer: JSONPointer(pathComponents: ["baz"]), encodableValue: "boo"),
+            expected: Model(baz: "boo", foo: [])
+        ),
+        (
+            input: Model(baz: "qux", foo: [1, 2]),
+            patch: .replace(pointer: JSONPointer(pathComponents: ["foo", "1"]), encodableValue: 123),
+            expected: Model(baz: "qux", foo: [1, 123])
+        ),
+        (
+            input: Model(baz: "qux", bar: [.init(bar: "value")]),
+            patch: .replace(pointer: JSONPointer(pathComponents: ["bar", "0", "bar"]), encodableValue: "new value"),
+            expected: Model(baz: "qux", bar: [.init(bar: "new value")])
+        ),
+        (
+            input: Model(baz: "qux"),
+            patch: .remove(pointer: JSONPointer(pathComponents: ["baz"])),
+            expected: Model(baz: nil)
+        ),
+        (
+            input: Model(baz: "qux", foo: [8, 9]),
+            patch: .remove(pointer: JSONPointer(pathComponents: ["foo", "0"])),
+            expected: Model(baz: "qux", foo: [9])
+        ),
+        (
+            input: Model(),
+            patch: .add(pointer: JSONPointer(pathComponents: ["baz"]), encodableValue: "qux"),
+            expected: Model(baz: "qux")
+        ),
+        (
+            input: Model(bar: [.init()]),
+            patch: .add(pointer: JSONPointer(pathComponents: ["bar", "0", "foo"]), encodableValue: "foo"),
+            expected: Model(bar: [.init(foo: "foo")])
+        ),
+        (
+            input: Model(bar: [.init()]),
+            patch: .add(pointer: JSONPointer(pathComponents: ["bar", "0"]), encodableValue: Model.Model(foo: "foo")),
+            expected: Model(bar: [.init(foo: "foo"), .init()])
+        ),
+        (
+            input: Model(bar: [.init()]),
+            patch: .add(pointer: JSONPointer(pathComponents: ["bar", "1"]), encodableValue: Model.Model(foo: "foo")),
+            expected: Model(bar: [.init(), .init(foo: "foo")])
+        ),
+    ])
+    func appliesPatchOperation(input: Model, patch: JSONPatchOperation, expected: Model) throws {
+        #expect(try apply(patch, to: input) == expected)
     }
     
-    func testReplacesArrayValue() throws {
-        XCTAssertEqual(
-            try apply(
-                .replace(pointer: JSONPointer(pathComponents: ["foo", "1"]), encodableValue: 123),
-                to: Model(
-                    baz: "qux",
-                    foo: [1, 2]
-                )
-            ),
-            Model(
-                baz: "qux",
-                foo: [1, 123]
-            )
-        )
-    }
-    
-    func testReplacesNestedValue() throws {
-        XCTAssertEqual(
-            try apply(
-                .replace(
-                    pointer: JSONPointer(pathComponents: ["bar", "0", "bar"]),
-                    encodableValue: "new value"
-                ),
-                to: Model(
-                    baz: "qux",
-                    bar: [.init(bar: "value")]
-                )
-            ),
-            Model(
-                baz: "qux",
-                bar: [.init(bar: "new value")]
-            )
-        )
-    }
-    
-    func testRemovesDictionaryValue() throws {
-        XCTAssertEqual(
-            try apply(
-                .remove(pointer: JSONPointer(pathComponents: ["baz"])),
-                to: Model(baz: "qux")
-            ),
-            Model(baz: nil)
-        )
-    }
-    
-    func testRemovesArrayValue() throws {
-        XCTAssertEqual(
-            try apply(
-                .remove(pointer: JSONPointer(pathComponents: ["foo", "0"])),
-                to: Model(
-                    baz: "qux",
-                    foo: [8, 9]
-                )
-            ),
-            Model(
-                baz: "qux",
-                foo: [9]
-            )
-        )
-    }
-    
-    func testAddsDictionaryValue() throws {
-        XCTAssertEqual(
-            try apply(
-                .add(pointer: JSONPointer(pathComponents: ["baz"]), encodableValue: "qux"),
-                to: Model()
-            ),
-            Model(baz: "qux")
-        )
-    }
-
-    func testAddsNestedDictionaryValue() throws {
-        XCTAssertEqual(
-            try apply(
-                .add(pointer: JSONPointer(pathComponents: ["bar", "0", "foo"]), encodableValue: "foo"),
-                to: Model(bar: [.init()])
-            ),
-            Model(bar: [.init(foo: "foo")])
-        )
-    }
-    
-    func testAddsArrayValue() throws {
-        XCTAssertEqual(
-            try apply(
-                .add(pointer: JSONPointer(pathComponents: ["bar", "0"]), encodableValue: Model.Model(foo: "foo")),
-                to: Model(bar: [.init()])
-            ),
-            Model(bar: [.init(foo: "foo"), .init()])
-        )
-    }
-    
-    func testAddsArrayValueAtTheEnd() throws {
-        XCTAssertEqual(
-            try apply(
-                .add(pointer: JSONPointer(pathComponents: ["bar", "1"]), encodableValue: Model.Model(foo: "foo")),
-                to: Model(bar: [.init()])
-            ),
-            Model(bar: [.init(), .init(foo: "foo")])
-        )
-    }
-
-    func testAppliesMultiplePatchOperations() throws {
-        XCTAssertEqual(
+    @Test
+    func appliesMultiplePatchOperations() throws {
+        #expect(
             try apply(
                 .remove(pointer: JSONPointer(pathComponents: ["foo", "0"])),
                 .replace(pointer: JSONPointer(pathComponents: ["baz"]), encodableValue: "boo"),
@@ -136,81 +74,40 @@ class JSONPatchApplierTests: XCTestCase {
                     baz: "qux",
                     foo: [8, 9]
                 )
-            ),
-            Model(
+            ) == Model(
                 baz: "boo",
                 foo: [9]
             )
         )
     }
     
-    func testThrowsErrorForInvalidRemoveObjectPointer() throws {
-        XCTAssertThrowsError(
-            try apply(
-                .remove(pointer: JSONPointer(pathComponents: ["bar", "0", "invalid-property"])),
-                to: Model(
-                    baz: "qux",
-                    foo: [8, 9]
-                )
-            )
-        ) { error in
-            XCTAssertEqual(
-                error.localizedDescription,
-                "Invalid array pointer '/bar/0/invalid-property'. The index '0' is not valid for array of 0 elements."
-            )
+    @Test(arguments: [
+        (
+            input: Model(baz: "qux", foo: [8, 9]),
+            patch: JSONPatchOperation.remove(pointer: JSONPointer(pathComponents: ["bar", "0", "invalid-property"])),
+            expectedError: "Invalid array pointer '/bar/0/invalid-property'. The index '0' is not valid for array of 0 elements."
+        ),
+        (
+            input: Model(baz: "qux", foo: [8, 9]),
+            patch: .remove(pointer: JSONPointer(pathComponents: ["foo", "5"])),
+            expectedError: "Invalid array pointer '/foo/5'. The index '5' is not valid for array of 2 elements."
+        ),
+        (
+            input: Model(foo: [8, 9]),
+            patch: .add(pointer: JSONPointer(pathComponents: ["foo", "5"]), encodableValue: ""),
+            expectedError: "Invalid array pointer '/foo/5'. The index '5' is not valid for array of 2 elements."
+        ),
+        (
+            input: Model(baz: "qux", foo: [8, 9]),
+            patch: .remove(pointer: JSONPointer(pathComponents: ["baz", "5"])),
+            expectedError: #"Invalid value pointer '/baz/5'. The component '5' is not valid for the non-traversable value '"qux"'."#
+        ),
+    ])
+    func throwsErrorForInvalidPointer(input: Model, patch: JSONPatchOperation, expectedError: String) throws {
+        let error = #expect(throws: (any Error).self) {
+            try apply(patch, to: input)
         }
-    }
-    
-    func testThrowsErrorForInvalidRemoveArrayPointer() throws {
-        XCTAssertThrowsError(
-            try apply(
-                .remove(pointer: JSONPointer(pathComponents: ["foo", "5"])),
-                to: Model(
-                    baz: "qux",
-                    foo: [8, 9]
-                )
-            )
-        ) { error in
-            XCTAssertEqual(
-                error.localizedDescription,
-                "Invalid array pointer '/foo/5'. The index '5' is not valid for array of 2 elements."
-            )
-        }
-    }
-    
-    func testThrowsErrorForInvalidAddArrayPointer() throws {
-        XCTAssertThrowsError(
-            try apply(
-                .add(pointer: JSONPointer(pathComponents: ["foo", "5"]), encodableValue: ""),
-                to: Model(
-                    foo: [8, 9]
-                )
-            )
-        ) { error in
-            XCTAssertEqual(
-                error.localizedDescription,
-                "Invalid array pointer '/foo/5'. The index '5' is not valid for array of 2 elements."
-            )
-        }
-    }
-    
-    func testThrowsErrorForInvalidValuePointer() throws {
-        XCTAssertThrowsError(
-            try apply(
-                .remove(pointer: JSONPointer(pathComponents: ["baz", "5"])),
-                to: Model(
-                    baz: "qux",
-                    foo: [8, 9]
-                )
-            )
-        ) { error in
-            XCTAssertEqual(
-                error.localizedDescription,
-                """
-                Invalid value pointer '/baz/5'. The component '5' is not valid for the non-traversable value '"qux"'.
-                """
-            )
-        }
+        #expect(error?.localizedDescription == expectedError)
     }
     
     private func apply(_ patch: JSONPatchOperation..., to model: Model) throws -> Model {
@@ -220,7 +117,7 @@ class JSONPatchApplierTests: XCTestCase {
         )
     }
     
-    private struct Model: Codable, Equatable {
+    struct Model: Codable, Equatable {
         var baz: String?
         var foo: [Int] = []
         var bar: [Model] = []
