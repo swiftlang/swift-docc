da7cbb8ba559658b1a194487912173c6dba27fd4
David Rönnqvist <ronnqvist@apple.com>
2026-06-15 14:52:28 +0200

    Remove API that was deprecated during the 6.3 release (#1550)
    * Remove API that was deprecated during the 6.3 release
    
    * Remove `try` for expression in test that doesn't throw

--- Sources/SwiftDocC/Model/Rendering/DocumentationContentRenderer.swift
+++ Sources/SwiftDocC/Model/Rendering/DocumentationContentRenderer.swift
@@ -58,11 +58,6 @@ public class DocumentationContentRenderer {
         self.urlGenerator = PresentationURLGenerator(context: context, baseURL: context.inputs.baseURL)
     }
     
-    @available(*, deprecated, renamed: "init(context:)", message: "Use 'init(context:)' instead. This deprecated API will be removed after 6.4 is released.")
-    public convenience init(documentationContext: DocumentationContext, bundle _: DocumentationBundle) {
-        self.init(context: documentationContext)
-    }
-    
     /// For symbol nodes, returns the declaration render section if any.
     func subHeadingFragments(for node: DocumentationNode) -> VariantCollection<[DeclarationRenderSection.Token]?> {
         guard let symbol = (node.semantic as? Symbol) else {
