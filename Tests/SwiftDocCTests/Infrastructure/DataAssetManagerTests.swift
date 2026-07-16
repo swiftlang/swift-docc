28eec3d01800aec0afcf275b225f2391b7d181a7
Prajwal Nadig <pnadig@apple.com>
2026-07-16 22:10:35 +0100

    Migrate data asset manager tests to Swift Testing

--- Tests/SwiftDocCTests/Infrastructure/DataAssetManagerTests.swift
+++ Tests/SwiftDocCTests/Infrastructure/DataAssetManagerTests.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2026 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See https://swift.org/LICENSE.txt for license information
@@ -9,38 +9,39 @@
 */
 
 import Foundation
-import XCTest
+import Testing
 @testable import SwiftDocC
 
-class DataAssetManagerTests: XCTestCase {
-    
-    func assertIsRegistered(named name: String, trait: DataTraitCollection, expectedURL: URL, manager: DataAssetManager, file: StaticString = #filePath, line: UInt = #line) {
-        let data = manager.data(named: name, bestMatching: trait)!
-        XCTAssertEqual(data.url, expectedURL)
-        XCTAssertEqual(data.traitCollection, trait)
+struct DataAssetManagerTests {
+
+    func assertIsRegistered(named name: String, trait: DataTraitCollection, expectedURL: URL, manager: DataAssetManager, sourceLocation: SourceLocation = #_sourceLocation) throws {
+        let data = try #require(manager.data(named: name, bestMatching: trait), sourceLocation: sourceLocation)
+        #expect(data.url == expectedURL, sourceLocation: sourceLocation)
+        #expect(data.traitCollection == trait, sourceLocation: sourceLocation)
     }
-    
-    func testWithoutDarkVariants() throws {
+
+    @Test
+    func registersImagesWithoutDarkVariants() throws {
         var manager = DataAssetManager()
         let images = ["Documentation/woof.png", "bark.jpg", "wuphf.jpeg", "woof.png"].compactMap(URL.init(string:))
         try manager.register(data: images)
-        
-        XCTAssertEqual(manager.storage.values.count, 3)
-        
+
+        #expect(manager.storage.values.count == 3)
+
         // The asset manager should contain all the images.
-        
-        assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[1], manager: manager)
-        assertIsRegistered(named: "wuphf.jpeg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[2], manager: manager)
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[3], manager: manager)
-        
+        try assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[1], manager: manager)
+        try assertIsRegistered(named: "wuphf.jpeg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[2], manager: manager)
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[3], manager: manager)
+
         // The asset manager should contain not dark images.
-        
-        XCTAssertEqual(manager.data(named: "bark.jpg", bestMatching: .init(userInterfaceStyle: .dark))?.traitCollection, .init(userInterfaceStyle: .light, displayScale: .standard))
-        XCTAssertEqual(manager.data(named: "wuphf.jpeg", bestMatching: .init(userInterfaceStyle: .dark))?.traitCollection, .init(userInterfaceStyle: .light, displayScale: .standard))
-        XCTAssertEqual(manager.data(named: "woof.png", bestMatching: .init(userInterfaceStyle: .dark))?.traitCollection, .init(userInterfaceStyle: .light, displayScale: .standard))
+        let expectedFallback = DataTraitCollection(userInterfaceStyle: .light, displayScale: .standard)
+        for name in ["bark.jpg", "wuphf.jpeg", "woof.png"] {
+            #expect(manager.data(named: name, bestMatching: .init(userInterfaceStyle: .dark))?.traitCollection == expectedFallback)
+        }
     }
-    
-    func testWithDarkVariants() throws {
+
+    @Test
+    func registersLightAndDarkVariants() throws {
         var manager = DataAssetManager()
         let images = [
             "Documentation/woof.png",
@@ -51,22 +52,22 @@ class DataAssetManagerTests: XCTestCase {
             "wuphf~dark.jpeg",
         ].compactMap(URL.init(string:))
         try manager.register(data: images)
-        
-        XCTAssertEqual(manager.storage.values.count, 3)
-        
+
+        #expect(manager.storage.values.count == 3)
+
         // The asset manager should contain all the light and dark variants.
-        
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[0], manager: manager)
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[1], manager: manager)
-        
-        assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[2], manager: manager)
-        assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[3], manager: manager)
-        
-        assertIsRegistered(named: "wuphf.jpeg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[4], manager: manager)
-        assertIsRegistered(named: "wuphf.jpeg", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[5], manager: manager)
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[0], manager: manager)
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[1], manager: manager)
+
+        try assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[2], manager: manager)
+        try assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[3], manager: manager)
+
+        try assertIsRegistered(named: "wuphf.jpeg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[4], manager: manager)
+        try assertIsRegistered(named: "wuphf.jpeg", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[5], manager: manager)
     }
-    
-    func testImageDisplayScale() throws {
+
+    @Test
+    func registersDisplayScaleVariants() throws {
         var manager = DataAssetManager()
         let images = [
             "woof.png",
@@ -80,114 +81,115 @@ class DataAssetManagerTests: XCTestCase {
             "bark~dark@2x.jpg",
         ].compactMap(URL.init(string:))
         try manager.register(data: images)
-        
-        XCTAssertEqual(manager.storage.values.count, 2)
-        
+
+        #expect(manager.storage.values.count == 2)
+
         // The asset manager should contain all the light and dark variants, plus pixel density versions.
-        
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[0], manager: manager)
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[1], manager: manager)
-        
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .dark, displayScale: .double), expectedURL: images[2], manager: manager)
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .dark, displayScale: .triple), expectedURL: images[3], manager: manager)
-        
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .double), expectedURL: images[4], manager: manager)
-        assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .triple), expectedURL: images[5], manager: manager)
-        
-        assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[6], manager: manager)
-        assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[7], manager: manager)
-        assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .dark, displayScale: .double), expectedURL: images[8], manager: manager)
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[0], manager: manager)
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[1], manager: manager)
+
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .dark, displayScale: .double), expectedURL: images[2], manager: manager)
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .dark, displayScale: .triple), expectedURL: images[3], manager: manager)
+
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .double), expectedURL: images[4], manager: manager)
+        try assertIsRegistered(named: "woof.png", trait: .init(userInterfaceStyle: .light, displayScale: .triple), expectedURL: images[5], manager: manager)
+
+        try assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: images[6], manager: manager)
+        try assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .dark, displayScale: .standard), expectedURL: images[7], manager: manager)
+        try assertIsRegistered(named: "bark.jpg", trait: .init(userInterfaceStyle: .dark, displayScale: .double), expectedURL: images[8], manager: manager)
     }
-    
-    func testTraitCollection() {
+
+    @Test
+    func mergesTraitCollections() {
         let lightTrait = DataTraitCollection(userInterfaceStyle: .light)
         let darkTrait = DataTraitCollection(userInterfaceStyle: .dark, displayScale: .double)
         let trait = DataTraitCollection(traitsFrom: [lightTrait, darkTrait])
-        
-        XCTAssertEqual(trait.userInterfaceStyle, .dark)
-        XCTAssertEqual(trait.displayScale, .double)
-        
+
+        #expect(trait.userInterfaceStyle == .dark)
+        #expect(trait.displayScale == .double)
+
         let trait2 = DataTraitCollection(traitsFrom: [lightTrait, darkTrait, lightTrait])
-        
-        XCTAssertEqual(trait2.userInterfaceStyle, .light)
-        XCTAssertEqual(trait2.displayScale, .double)
+
+        #expect(trait2.userInterfaceStyle == .light)
+        #expect(trait2.displayScale == .double)
     }
-    
+
     // Under the default settings this test will use `NSImage` under macOS
-    func testImageSize() throws {
-        let imageFileURL = Bundle.module.url(
-            forResource: "image", withExtension: "png", subdirectory: "Test Resources")!
-        XCTAssertTrue(FileManager.default.fileExists(atPath: imageFileURL.path))
+    @Test
+    func registersImageFromDisk() throws {
+        let imageFileURL = try #require(
+            Bundle.module.url(forResource: "image", withExtension: "png", subdirectory: "Test Resources")
+        )
+        #expect(FileManager.default.fileExists(atPath: imageFileURL.path))
 
         var manager = DataAssetManager()
-        
+
         // Register an image asset
         try manager.register(data: [imageFileURL])
 
         // Check the asset is registered
-        guard !manager.storage.values.isEmpty else {
-            XCTFail("Failed to register image asset")
-            return
-        }
-        assertIsRegistered(named: imageFileURL.lastPathComponent, trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: imageFileURL, manager: manager)
+        try assertIsRegistered(named: imageFileURL.lastPathComponent, trait: .init(userInterfaceStyle: .light, displayScale: .standard), expectedURL: imageFileURL, manager: manager)
     }
-    
-    func testUpdateAsset() throws {
+
+    @Test
+    func updatesRegisteredAsset() throws {
         var manager = DataAssetManager()
         let images = [URL(string: "woof.png")!]
         try manager.register(data: images)
-        
-        XCTAssertEqual(manager.storage.count, 1)
-        
+
+        #expect(manager.storage.count == 1)
+
         // Fetch the asset.
-        guard var resolvedAsset = manager.allData(named: "woof.png") else {
-            XCTFail("Failed to resolve registered asset")
-            return
-        }
-        
+        var resolvedAsset = try #require(manager.allData(named: "woof.png"))
+
         // Modify the asset
         resolvedAsset.context = .download
-        
+
         // Update the manager storage
         manager.update(name: "woof.png", asset: resolvedAsset)
-        
+
         // Fetch the updated asset.
-        guard let updatedAsset = manager.allData(named: "woof.png") else {
-            XCTFail("Failed to resolve updated asset")
-            return
-        }
+        let updatedAsset = try #require(manager.allData(named: "woof.png"))
 
         // Verify it's the up to date asset.
-        XCTAssertEqual(updatedAsset.context, .download)
+        #expect(updatedAsset.context == .download)
     }
 
-    func testNonExistingAssets() throws {
+    @Test(arguments: [
+        (name: "blip", isRegistered: false),
+        (name: "image.jpg", isRegistered: false),
+        (name: "image.png", isRegistered: true),
+    ])
+    func resolvesOnlyRegisteredAssets(name: String, isRegistered: Bool) throws {
         var manager = DataAssetManager()
-        let images = ["image.png"].compactMap(URL.init(string:))
-        try manager.register(data: images)
-        
-        XCTAssertNil(manager.allData(named: "blip"))
-        XCTAssertNil(manager.allData(named: "image.jpg"))
-        XCTAssertNotNil(manager.allData(named: "image.png"))
+        try manager.register(data: ["image.png"].compactMap(URL.init(string:)))
+
+        if isRegistered {
+            #expect(manager.allData(named: name) != nil)
+        } else {
+            #expect(manager.allData(named: name) == nil)
+        }
     }
-    
-    func testLoadsImagesWithIdenticalNameSuffixes() throws {
+
+    @Test
+    func loadsImagesWithIdenticalNameSuffixes() throws {
         var manager = DataAssetManager()
         let images = [
             "image.png",
             "assets/different_image.png",
         ].compactMap(URL.init(string:))
         try manager.register(data: images)
-        
-        XCTAssertEqual(manager.allData(named: "image")?.variants.first?.value.path, "image.png")
-        XCTAssertEqual(manager.allData(named: "image.png")?.variants.first?.value.path, "image.png")
-        
-        XCTAssertEqual(manager.allData(named: "different_image")?.variants.first?.value.path, "assets/different_image.png")
-        XCTAssertEqual(manager.allData(named: "different_image.png")?.variants.first?.value.path, "assets/different_image.png")
-        XCTAssertNil(manager.allData(named: "assets/different_image.png"))
+
+        #expect(manager.allData(named: "image")?.variants.first?.value.path == "image.png")
+        #expect(manager.allData(named: "image.png")?.variants.first?.value.path == "image.png")
+
+        #expect(manager.allData(named: "different_image")?.variants.first?.value.path == "assets/different_image.png")
+        #expect(manager.allData(named: "different_image.png")?.variants.first?.value.path == "assets/different_image.png")
+        #expect(manager.allData(named: "assets/different_image.png") == nil)
     }
-    
-    func testFuzzyLookup() throws {
+
+    @Test
+    func fuzzyLookupMatchesNameWithoutExtension() throws {
         var manager = DataAssetManager()
         let images = [
             "image.png",
@@ -196,25 +198,26 @@ class DataAssetManagerTests: XCTestCase {
         try manager.register(data: images)
 
         // The fuzzy lookup will match "name" to "name.png"
-        XCTAssertEqual(manager.allData(named: "image")?.variants.first?.value.path, "image.png")
+        #expect(manager.allData(named: "image")?.variants.first?.value.path == "image.png")
         // But not "name.ext1" to "name.ext2"
-        XCTAssertNil(manager.allData(named: "image.JPG"))
-        
-        XCTAssertNil(manager.allData(named: "woof.jpg"))
-        XCTAssertEqual(manager.allData(named: "woof.JPG")?.variants.first?.value.path, "woof~dark.JPG")
-        XCTAssertEqual(manager.allData(named: "woof")?.variants.first?.value.path, "woof~dark.JPG")
+        #expect(manager.allData(named: "image.JPG") == nil)
+
+        #expect(manager.allData(named: "woof.jpg") == nil)
+        #expect(manager.allData(named: "woof.JPG")?.variants.first?.value.path == "woof~dark.JPG")
+        #expect(manager.allData(named: "woof")?.variants.first?.value.path == "woof~dark.JPG")
     }
-    
-    func testFuzzyLookupIndex() throws {
+
+    @Test
+    func buildsFuzzyKeyIndexWithUniqueKeys() throws {
         var manager = DataAssetManager()
-        
+
         // Test that we build the fuzzy index
         let images = [
             "image@2x.png",
             "assets/woof.JPG",
         ].compactMap(URL.init(string:))
         try manager.register(data: images)
-        XCTAssertEqual(manager.fuzzyKeyIndex.keys.sorted().map({"\($0)"}), ["image", "woof"])
+        #expect(manager.fuzzyKeyIndex.keys.sorted().map({ "\($0)" }) == ["image", "woof"])
 
         // Test we include only unique keys in the fuzzy index
         let imagesWithDuplicates = [
@@ -223,6 +226,6 @@ class DataAssetManagerTests: XCTestCase {
             "woof.png",
         ].compactMap(URL.init(string:))
         try manager.register(data: imagesWithDuplicates)
-        XCTAssertEqual(manager.fuzzyKeyIndex.keys.sorted().map({"\($0)"}), ["image", "woof"])
+        #expect(manager.fuzzyKeyIndex.keys.sorted().map({ "\($0)" }) == ["image", "woof"])
     }
 }
