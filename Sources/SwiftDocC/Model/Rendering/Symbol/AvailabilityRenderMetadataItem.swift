20a3cd40b1e39cf0f0515067da72e310dd3a8a95
David Rönnqvist <ronnqvist@apple.com>
2026-09-11 17:11:06 +0200

    Deprecate unused render availability information (#1656)

--- Sources/SwiftDocC/Model/Rendering/Symbol/AvailabilityRenderMetadataItem.swift
+++ Sources/SwiftDocC/Model/Rendering/Symbol/AvailabilityRenderMetadataItem.swift
@@ -66,22 +66,42 @@ public struct AvailabilityRenderItem: Codable, Hashable, Equatable {
     public var deprecated: String?
     
     /// The version of the platform SDK marking the symbol as obsolete.
-    public var obsoleted: String?
+    @available(*, deprecated, message: "Obsoleted platforms are not included. This deprecated API will be removed after 6.6 is released")
+    public var obsoleted: String? {
+        get { _obsoleted }
+        set { _obsoleted = newValue }
+    }
+    private var _obsoleted: String?
     
     /// A message associated with the availability of the symbol.
     ///
     /// Use this property to provide a deprecation reason or instructions how to
     /// update code that uses this symbol.
-    public var message: String?
+    @available(*, deprecated, message: "This information is unused. This deprecated API will be removed after 6.6 is released")
+    public var message: String? {
+        get { _message }
+        set { _message = newValue }
+    }
+    private var _message: String?
     
     /// The new name of the symbol, if it was renamed.
-    public var renamed: String?
+    @available(*, deprecated, message: "This information is unused. This deprecated API will be removed after 6.6 is released")
+    public var renamed: String? {
+        get { _renamed }
+        set { _renamed = newValue }
+    }
+    private var _renamed: String?
     
     /// If `true`, the symbol is deprecated on this or all platforms.
     public var unconditionallyDeprecated: Bool?
     
     /// If `true`, the symbol is unavailable on this or all platforms.
-    public var unconditionallyUnavailable: Bool?
+    @available(*, deprecated, message: "Unavailable platforms are not included. This deprecated API will be removed after 6.6 is released")
+    public var unconditionallyUnavailable: Bool? {
+        get { _unconditionallyUnavailable }
+        set { _unconditionallyUnavailable = newValue }
+    }
+    private var _unconditionallyUnavailable: Bool?
     
     /// If `true`, the symbol is introduced in a beta version of this platform.
     public var isBeta: Bool?
@@ -96,11 +116,11 @@ public struct AvailabilityRenderItem: Codable, Hashable, Equatable {
         name = try container.decodeIfPresent(String.self, forKey: .name)
         introduced = try container.decodeIfPresent(String.self, forKey: .introducedAt)
         deprecated = try container.decodeIfPresent(String.self, forKey: .deprecatedAt)
-        obsoleted = try container.decodeIfPresent(String.self, forKey: .obsoletedAt)
-        message = try container.decodeIfPresent(String.self, forKey: .message)
-        renamed = try container.decodeIfPresent(String.self, forKey: .renamed)
+        _obsoleted = try container.decodeIfPresent(String.self, forKey: .obsoletedAt)
+        _message = try container.decodeIfPresent(String.self, forKey: .message)
+        _renamed = try container.decodeIfPresent(String.self, forKey: .renamed)
         unconditionallyDeprecated = try container.decodeIfPresent(Bool.self, forKey: .deprecated)
-        unconditionallyUnavailable = try container.decodeIfPresent(Bool.self, forKey: .unavailable)
+        _unconditionallyUnavailable = try container.decodeIfPresent(Bool.self, forKey: .unavailable)
         isBeta = try container.decodeIfPresent(Bool.self, forKey: .beta)
     }
     
@@ -110,11 +130,11 @@ public struct AvailabilityRenderItem: Codable, Hashable, Equatable {
         try container.encodeIfPresent(name, forKey: .name)
         try container.encodeIfPresent(introduced, forKey: .introducedAt)
         try container.encodeIfPresent(deprecated, forKey: .deprecatedAt)
-        try container.encodeIfPresent(obsoleted, forKey: .obsoletedAt)
-        try container.encodeIfPresent(message, forKey: .message)
-        try container.encodeIfPresent(renamed, forKey: .renamed)
+        try container.encodeIfPresent(_obsoleted, forKey: .obsoletedAt)
+        try container.encodeIfPresent(_message, forKey: .message)
+        try container.encodeIfPresent(_renamed, forKey: .renamed)
         try container.encodeIfPresent(unconditionallyDeprecated, forKey: .deprecated)
-        try container.encodeIfPresent(unconditionallyUnavailable, forKey: .unavailable)
+        try container.encodeIfPresent(_unconditionallyUnavailable, forKey: .unavailable)
         try container.encodeIfPresent(isBeta, forKey: .beta)
     }
     
@@ -128,10 +148,10 @@ public struct AvailabilityRenderItem: Codable, Hashable, Equatable {
         let introducedVersion = availability.introducedVersion.flatMap { SemanticVersion($0) }
         introduced = introducedVersion?.stringRepresentation(precisionUpToNonsignificant: .minor)
         deprecated = availability.deprecatedVersion.flatMap { SemanticVersion($0).stringRepresentation(precisionUpToNonsignificant: .minor) }
-        obsoleted = availability.obsoletedVersion.flatMap { SemanticVersion($0).stringRepresentation(precisionUpToNonsignificant: .minor) }
-        message = availability.message
-        renamed = availability.renamed
-        unconditionallyUnavailable = availability.isUnconditionallyUnavailable
+        _obsoleted = availability.obsoletedVersion.flatMap { SemanticVersion($0).stringRepresentation(precisionUpToNonsignificant: .minor) }
+        _message = availability.message
+        _renamed = availability.renamed
+        _unconditionallyUnavailable = availability.isUnconditionallyUnavailable
         unconditionallyDeprecated = availability.isUnconditionallyDeprecated
         isBeta = AvailabilityRenderItem.isBeta(introduced: introducedVersion, current: current)
     }
@@ -166,11 +186,11 @@ public struct AvailabilityRenderItem: Codable, Hashable, Equatable {
         self.name = name
         self.introduced = introduced
         self.deprecated = deprecated
-        self.obsoleted = obsoleted
-        self.message = message
-        self.renamed = renamed
+        self._obsoleted = obsoleted
+        self._message = message
+        self._renamed = renamed
         self.unconditionallyDeprecated = unconditionallyDeprecated
-        self.unconditionallyUnavailable = unconditionallyUnavailable
+        self._unconditionallyUnavailable = unconditionallyUnavailable
         self.isBeta = isBeta
     }
 
