2d304b0f55946e058b141a39a29f0a22b39e5468
Prajwal Nadig <pnadig@apple.com>
2026-09-14 15:40:45 +0100

    Collapse some iterator chains

--- Sources/SwiftDocC/Indexing/RenderBlockContent+TextIndexing.swift
+++ Sources/SwiftDocC/Indexing/RenderBlockContent+TextIndexing.swift
@@ -71,9 +71,8 @@ extension RenderBlockContent: TextIndexing {
         case .links(let links):
             // Matches the behavior in `RenderInlineContent+TextIndexing` for a
             // `RenderInlineContent.reference`
-            return links.items.lazy
-                .compactMap { references[$0] as? TopicRenderReference }
-                .map(\.title)
+            return links.items
+                .compactMap { (references[$0] as? TopicRenderReference)?.title }
                 .joined(separator: " ")
         case .video(let video):
             return video.metadata?.rawIndexableTextContent(references: references) ?? ""
