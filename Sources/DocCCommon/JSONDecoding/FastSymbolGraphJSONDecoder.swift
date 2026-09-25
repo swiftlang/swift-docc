700457d81b163aa7f1dc84f15cbcacfc475cff42
David Rönnqvist <ronnqvist@apple.com>
2026-09-25 18:27:39 +0200

    Restore performance of common array decoding.

--- Sources/DocCCommon/JSONDecoding/FastSymbolGraphJSONDecoder.swift
+++ Sources/DocCCommon/JSONDecoding/FastSymbolGraphJSONDecoder.swift
@@ -391,39 +391,16 @@ package struct FastSymbolGraphJSONDecoder: ~Copyable {
     /// - Returns: The decoded values.
     @inlinable
     package mutating func decode<Value: FastJSONDecodable>(_ type: [Value].Type) throws(DecodingError) -> [Value] {
-        try _decodeArray { decoder throws(DecodingError) -> Value in
-            try decoder.decode(Value.self)
-        }
-    }
-    
-    /// Dynamically decodes an array of values and advances the decoder's state.
-    ///
-    /// It is recommended to call `decode([Value].self)` instead of calling this method whenever possible. Doing so is both easier and safer (less risk to get the decoder in an invalid state).
-    /// However, sometimes---for example when decoding "existential types" (`any Value`)---it's not possible to conform to `FastJSONDecodable` but it is possible to manually decode a value.
-    package mutating func _decodeArray<Value>(_ body: (inout Self) throws(DecodingError) -> Value) throws(DecodingError) -> [Value] {
         do {
-            try _descendIntoArray()
-            
-            // Add a new index to the decoder's path.
-            path.push(.index(0))
+            try _commonArrayDecodingPreamble()
             
             var values: [Value] = []
-            _skipWhitespace()
-            
             while pointer.nextByte != .init(ascii: "]") {
-                values.append(try body(&self))
-                path.incrementCurrentIndex()
+                values.append(try self.decode(Value.self))
                 
-                _skipWhitespace()
-                if pointer.nextByte == .init(ascii: ",") {
-                    pointer.removeFirst()
-                    _skipWhitespace()
-                }
+                try _commonArrayDecodingIterationAdvancement()
             }
-            pointer.removeFirst()
-            path.pop()
-            
-            try _boundsCheck()
+            try _commonArrayDecodingCleanup()
             
             return values
         } catch ScanningError.unexpectedCharacter {
@@ -433,6 +410,58 @@ package struct FastSymbolGraphJSONDecoder: ~Copyable {
         } catch {
             throw makeGenericDataCorruptedError()
         }
+    }
+    
+    /// Dynamically decodes an array of values and advances the decoder's state.
+    ///
+    /// It is recommended to call `decode([Value].self)` instead of calling this method whenever possible. Doing so is both easier and safer (less risk to get the decoder in an invalid state).
+    /// However, sometimes---for example when decoding "existential types" (`any Value`)---it's not possible to conform to `FastJSONDecodable` but it is possible to manually decode a value.
+    package mutating func _decodeArray<Value>(_ body: (inout Self) throws(DecodingError) -> Value) throws(DecodingError) -> [Value] {
+        do {
+            try _commonArrayDecodingPreamble()
+            
+            var values: [Value] = []
+            while pointer.nextByte != .init(ascii: "]") {
+                values.append(try body(&self))
+                
+                try _commonArrayDecodingIterationAdvancement()
+            }
+            try _commonArrayDecodingCleanup()
+            
+            return values
+        } catch ScanningError.unexpectedCharacter {
+            throw makeTypeMismatchError(Array<Any>.self) // We're dropping some type information here to match JSONDecoder's behavior
+        } catch let decodingError as DecodingError{
+            throw decodingError
+        } catch {
+            throw makeGenericDataCorruptedError()
+        }
+    }
+    
+    // Both ``decode(_:)->[Value]`` and ``_decodeArray(_:)`` do the same
+    // Unfortunately,
+    private mutating func _commonArrayDecodingPreamble() throws(ScanningError) {
+        try _descendIntoArray()
+        // The decoder's path is not pointing to the first position in the array.
+        path.push(.index(0))
+        _skipWhitespace()
+    }
+    
+    private mutating func _commonArrayDecodingIterationAdvancement() throws(DecodingError) {
+        path.incrementCurrentIndex()
+        
+        _skipWhitespace()
+        if pointer.nextByte == .init(ascii: ",") {
+            pointer.removeFirst()
+            _skipWhitespace()
+        }
+    }
+    
+    private mutating func _commonArrayDecodingCleanup() throws(ScanningError) {
+        pointer.removeFirst()
+        path.pop()
+        
+        try _boundsCheck()
     }
     
     /// Decodes an optional decodable values and advances the decoder's state.
